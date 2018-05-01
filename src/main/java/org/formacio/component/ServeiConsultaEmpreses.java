5d66eac9d8f48b05a61b24879efe697d664fb9f6
Adrián <adriangalende@gmail.com>
2018-05-01 10:14:57 +0200

    test_consulta_cotitzacio_empresa

--- src/main/java/org/formacio/component/ServeiConsultaEmpreses.java
+++ src/main/java/org/formacio/component/ServeiConsultaEmpreses.java
@@ -1,6 +1,7 @@
 package org.formacio.component;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // aquesta classe ha de ser detectada com un component
@@ -9,11 +10,13 @@ public class ServeiConsultaEmpreses {
 
 	// aquest servei necessita d'un integrador de cotitzacions per retornar la informacio
 	// declarau la dependencia i feis que spring la resolgui
+	@Autowired
+	private ClientCotitzacionsWS cotitzacionsWS;
 	
 	
 	// modifiqueu aquest metode per canviar el 0 per el valor obtingut del clientCotitzacionsWS
 	public String infoEmpresa(String empresa) {
-		return "La empresa " + empresa + " cotitza a 0";
+		return "La empresa " + empresa + " cotitza a " + cotitzacionsWS.obteCotitzacio(empresa);
 	}
 
 	
